8b4bfe4bb098e042731b65395dc2ea12eeff3b8f
Paulo Gomes <pvhgomes2003@gmail.com>
2026-06-11 21:03:31 -0300

    feat(persistence): implementar entidades jpa e interfaces de repositorio para os tres microsservicos

---
+++ services/academic-service/src/main/java/com/example/academic_service/domain/Disciplina.java
@@ -0,0 +1,42 @@
+package com.example.academic_service.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_disciplinas")
+public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String codigo; // Ex.: "ENG-SOFt", "CALC-II", etc.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
